dc021464a5838b287197c9cff12bb74e40e069f4
Tim Vučina <56194319+vucinatim@users.noreply.github.com>
2020-07-25 11:01:39 +0200

    Added basic display of todos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          description: "The todo description 1",
+          completed: false,
+          endDate: new Date(),
+          user: "user1",
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          description: "The todo description 2",
+          completed: true,
+          endDate: new Date(),
+          user: "user1",
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          description: "The todo description 3",
+          completed: false,
+          endDate: new Date(),
+          user: "user1",
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
